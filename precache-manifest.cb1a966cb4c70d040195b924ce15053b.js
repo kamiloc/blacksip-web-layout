39328aa35fd7d713cb7f975797c565d6d750757a
Cristian Barreto <kamilo.sniper@gmail.com>
2019-05-08 15:00:39 -0500

    Updates

---
+++ precache-manifest.cb1a966cb4c70d040195b924ce15053b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80275d71e984547dedab0c3d63aa1d6c",
+    "url": "/kamiloc/blacksip-web-layout/index.html"
+  },
+  {
+    "revision": "559ba94c47e1471e86ab",
+    "url": "/kamiloc/blacksip-web-layout/static/css/2.cdb4bf7c.chunk.css"
+  },
+  {
+    "revision": "89373e7ce2d786871422",
+    "url": "/kamiloc/blacksip-web-layout/static/css/main.81372dda.chunk.css"
+  },
+  {
+    "revision": "559ba94c47e1471e86ab",
+    "url": "/kamiloc/blacksip-web-layout/static/js/2.b83e4e51.chunk.js"
+  },
+  {
+    "revision": "89373e7ce2d786871422",
+    "url": "/kamiloc/blacksip-web-layout/static/js/main.61826a6b.chunk.js"
+  },
+  {
+    "revision": "672c91c65bde7366b44c",
+    "url": "/kamiloc/blacksip-web-layout/static/js/runtime~main.41ee168b.js"
+  },
+  {
+    "revision": "dda23577d1fa378154e335af0346e96f",
+    "url": "/kamiloc/blacksip-web-layout/static/media/2.dda23577.jpg"
+  },
+  {
+    "revision": "47b2a67d2e744bd5897145e79ed4711e",
+    "url": "/kamiloc/blacksip-web-layout/static/media/3.47b2a67d.jpg"
+  },
+  {
+    "revision": "7a0459b9ec288c10062e1ac915cd0254",
+    "url": "/kamiloc/blacksip-web-layout/static/media/4.7a0459b9.jpg"
+  },
+  {
+    "revision": "286ac73d9cdc7b628d5aa440a9ca39d1",
+    "url": "/kamiloc/blacksip-web-layout/static/media/chat.286ac73d.jpg"
+  },
+  {
+    "revision": "e31fe5becca35313c9d549555e464721",
+    "url": "/kamiloc/blacksip-web-layout/static/media/logo.e31fe5be.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/kamiloc/blacksip-web-layout/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/kamiloc/blacksip-web-layout/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/kamiloc/blacksip-web-layout/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/kamiloc/blacksip-web-layout/static/media/slick.f97e3bbf.svg"
+  }
+]);
